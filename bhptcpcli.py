52ad75afbd95079d956ec4f9c48f6c83df333ce6
n1cfury <fukugetin@gmail.com>
2017-01-31 08:32:19 -0800

    TCP Client.  This needs some functions.  Also, this does look familiar ;)

--- bhptcpcli.py
+++ bhptcpcli.py
@@ -1,27 +1,14 @@
  #!/ usr/bin/python
-
-#Black Hat Python
-#TCP Client
-#page 10
-
-/bin/bash: q: command not found
-#blackhat python
+#Black Hat Python: TCP Client, Pg 10
+#TODO:  ADD SOME FUNCTIONS.  ALSO, TAKE A LOOK AT SOME OTHER CODE YOU'VE PUT TOGETHER. THIS MAY LOOK FAMILIAR<HINT>.
 
 import socket
+
 target_host = "www.google.com"
 target_port = 80
 
-# create a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#Connect the client
-
-client.connect((target_host, target_port))
-
-#send some data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
-
-#receive some data
-response = client.recv(4096)
-
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         #Create a socket for the target host, port
+client.connect((target_host, target_port))                         #Connect the client
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")          #Sends Data
+response = client.recv(4096)                                       #Receives Data from socket connection
 print response
