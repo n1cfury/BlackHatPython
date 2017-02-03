446cfdc98b079fed330bd566046f7c52b9af964b
n1cfury <fukugetin@gmail.com>
2017-02-02 17:47:59 -0800

    Black Hat Python TCP Server

--- bhptcpsrv.py
+++ bhptcpsrv.py
@@ -3,10 +3,10 @@
 #Blackhat Python; TCP Server pg 12
 #TODO:  ADD SOME FUNCTIONS IN TO MAKE THIS WORK BETTER
 
-import socket, threading
+import socket, threading, sys
 
-bind_ip = "0.0.0.0"
-bind_port = 9999
+bind_ip = sys.argv[1]
+bind_port = sys.argv[2]
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip, bind_port))
@@ -19,12 +19,12 @@ print "[*] Listening on %s:%d" % (bind_ip, bind_port)
 def handle_client(client_socket):
     request = client_socket.recv(1024)
     print "[*] Received: %s" % request
-
     client_socket.send("ACK!")
     client_socket.close()
+	while True:
+	    client, addr = server.accept()
+	    print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
+	    client_handler = threading.Thread(target=handle_client, args=(client,))
+	    client_handler.start()
 
-while True:
-    client, addr = server.accept()
-    print "[*] Accepted connection from: %s:%d" % (addr[0], addr[1])
-    client_handler = threading.Thread(target=handle_client, args=(client,))
-    client_handler.start()
+#TODO: ADD CODE BELOW INSIDE THE MAIN FUNCTION.  THIS WILL MAKE THE DEFAULT bind_ip 
