c9406b5e83b1984fabab6a9235b7eabfc74aa56d
n1cfury <fukugetin@gmail.com>
2017-02-02 18:32:40 -0800

    Added Functions and arguments to the UDP tool and renamed it

---
+++ udpclient.py
@@ -0,0 +1,26 @@
+ #!/ usr/bin/python
+import socket, sys
+
+usage = "updcli.py target_host target_port"
+target_host = sys.argv[1]
+target_port = sys.argv[2]
+
+def banner():
+	print "##### UDP Client  #######"					#Blackhat Python UDP Client, pg 11
+	print ""
+
+def worker(target_host, target_port):
+	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	client.sendto("AAABBBCCC", (target_host, target_port))
+	data, addr = client.recvfrom(4096)
+	print data
+
+def main():
+	if sys.argv == 3:
+		banner()
+		worker(target_host, target_port)
+	else: 
+		print usage
+
+if __name__ = "__main__":
+	main()
