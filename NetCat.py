ab73fc7a3970898f466998d59904d54d1fa5959b
n1cfury <fukugetin@gmail.com>
2017-02-06 22:17:58 -0800

    Fixed some minor spacing issues and found a couple indentation problems

--- NetCat.py
+++ NetCat.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-
 import sys,socket,getopt,threading,subprocess
-
 listen              = False         #Defining global variables
 command             = False
 upload              = False
@@ -11,22 +9,20 @@ upload_dest         = ""
 port                = 0
 
 def banner():
-    print "############   n1cFury- NetCat tool   #################"
-    print "############  courtesy of Black Hat Python  ###########"
-    print ""
+    print "############   NetCat tool   #################"
+
 def usage():
-    print "  USAGE: NetCat.py -t target_host -p port                    "
-    print "-l   --Listen on [host]:[port] for incoming connections      "
-    print "-e   --execute=file_to_run  - execute a file                 "
-    print "-c   --command              - initialize a command shell     "
-    print "-u   --upload = destination - upload a file                  "
-    print "-t   --target    -p  --port                                  "
-    print ""
-    print "NetCat.py -t <target> -p 5555 -l -u=c:\\payload.exe          "
-    print "echo 'ABCDEFGHI' | ./NetCat.py -t 192.168.11.12 -p 135       "
-    print "./NetCat.py -l -p <port> (listens on a port)                 "
-    print "./NetCat.py -t <target> -p 9999 -c (CTRL+D opens cmd shell)  "
-    print "Press 'CTRL+D' to initalize shell after connecting           "
+    print "         NetCat.py -t target_host -p port                   "
+    print "-l   --Listen on [host]:[port] for incoming connections     "
+    print "-e   --execute = file_to_run  -execute a file               "
+    print "-c   --command              -initialize a command shell     "
+    print "-u   --upload = destination -upload a file                  "
+    print "-t   --target    -p  --port                                 "
+    print "NetCat.py -t <target> -p 5555 -l -u=c:\\payload.exe         "
+    print "echo 'ABCDEFGHI' | ./NetCat.py -t 192.168.11.12 -p 135      "
+    print "./NetCat.py -l -p <port> (listens on a port)                "
+    print "./NetCat.py -t <target> -p 9001 -c (CTRL+D opens cmd shell) "
+    print "Press 'CTRL+D' to initalize shell after connecting          "
 
 def run_command(command):               #trim the newline
     command = command.rstrip()          #run the command and get the output back
@@ -40,13 +36,12 @@ def client_handler(client_socket):
     global upload
     global execute
     global command
-
     if len(upload_dest):             #check for upload
         file_buffer = ""                    #read in all of the bytes and write to our destination
         while True:                         #keep reading data until none is available
             data = client_socket.recv(1024)
             if not data:
-                    break
+                break
             else:
                 file_buffer += data         #now we take these bytes and try to write them out
         try:                                       
@@ -56,63 +51,54 @@ def client_handler(client_socket):
             client_socket.send("Woohoo! File saved to %s\r\n" % upload_dest)
         except:
             client_socket.send("You suck! Your file didn't copy to %s\r\n" % upload_dest)
-
-    if len(execute):                          #click for command execution
-        output = run_command(execute)        #run the command
+    if len(execute):                                         #click for command execution
+        output = run_command(execute)                       #run the command
         client_socket.send(output)
-
-    if command:                             #going into a loop if a command shell was requested
+    if command:                                             #going into a loop if a command shell was requested
         while True:
             prompt = "<BHPNet:#> "
             client_socket.send(prompt)
-            cmd_buffer = ""                 #now we receive until we ses a linefeed(enter key)          
+            cmd_buffer = ""                                   #now we receive until we ses a linefeed(enter key)          
             while "\n" not in cmd_buffer:
                 cmd_buffer += client_socket.recv(1024)
-            response = run_command(cmd_buffer)             #send back the command output
-            client_socket.send(response)                   #send back the response
+            response = run_command(cmd_buffer)                 #send back the command output
+            client_socket.send(response)                     #send back the response
 
 def server_loop():
     global target
     global port
-    if not len(target):                     #if no target is defined, we listen on all interfaces
+    if not len(target):                                         #if no target is defined, we listen on all interfaces
         target = "0.0.0.0"
-
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target,port))
     server.listen(5)
-
     while True:
-        client_socket, addr = server.accept()          #spin off a thread to handle our new client
+        client_socket, addr = server.accept()                  #spin off a thread to handle our new client
         client_thread = threading.Thread(target=client_handler, args=(client_socket,))
         client_thread.start()
 
 def client_sender(buffer):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        #connect to our target host
-        client.connect((target, port))
+        client.connect((target, port))                      #connect to our target host
         if len(buffer):
             client.send(buffer)
-
-        while True:                      #Wait for the data back
+        while True:                                           #Wait for the data back
             recv_len = 1
             response = ""
-
             while recv_len:
                 data     = client.recv(4096)
                 recv_len = len(data)
                 response+= data
                 if recv_len < 4096:
                     break
-
             print response,    
-            buffer = raw_input("")      #wait for more input
+            buffer = raw_input("")                             #wait for more input
             buffer += "\n"
-            client.send(buffer)        #send it off
-
+            client.send(buffer)                               #send it off
     except:
         print "[*] Exception! Exiting."
-        client.close()                  #tear down the connection
+        client.close()                                         #tear down the connection
 
 def main():
     banner()
@@ -122,7 +108,6 @@ def main():
     global command
     global upload_dest
     global target
-
     if not len(sys.argv[1:]):
         usage()
     try:                            #reads command line options
@@ -131,7 +116,6 @@ def main():
     except getopt.GetoptError as err:
         print str(err)
         usage()
-
     for o,a in opts:                    #command options
         if o in ("-h", "--help"):
             usage()
