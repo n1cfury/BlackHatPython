c1909c7956a4addea496c7247b168896d8836e8b
n1cfury <fukugetin@gmail.com>
2017-02-15 20:40:32 -0800

    Updated banner.  DEBUG AND TEST

---
+++ sshCommand.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import threading, paramiko, subprocess
+
+usage = "./bh_sshcmd.py <hostname> <user> <password> <command>"
+host = sys.argv[1]
+user = sys.argv[2]
+passwd = sys.argv[3]
+command = sys.argv[4]
+
+def banner():
+    print "########     SSH Client p26    #########"			#Black Hat Python; SSH w/ Paramiko (p 26)
+    print "Single command client"
+    
+def ssh_command(ip, user, passwd, command):
+	client = paramiko.SSHClient()
+	#client.load_host_keys('/home/justin/.ssh/known_hosts')
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(ip, username= user, password= passwd)
+	ssh_session = client.get_transport().open_session()
+	if ssh_session.active:
+		ssh_session.exec_command(command)
+		print ssh_session.recv(1024) 										#reads the first 1024 bytes from the target host
+	return
+
+def main():
+	banner()
+	if sys.argv == 5:
+		ssh_command(ip, user, passwd, command)
+	else:
+		print usage
+
+if __name__ == "__main__":
+	main()
