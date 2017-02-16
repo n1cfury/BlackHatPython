bc27df32267a0e19bbccf7dc6293f39a73bb1d99
n1cfury <fukugetin@gmail.com>
2017-02-15 20:39:44 -0800

    Created empty functions

---
+++ BurpFuzzer.py
@@ -0,0 +1,27 @@
+
+
+def banner():
+print "Burp Fuzzer p79"
+
+class BurpExtender():
+
+	def registerExtenterCallbacks():
+
+	def getGeneratorName(self):
+
+	def createNewInstance():
+
+class BHPFuzzer():
+
+	def __init__(self, extender, attack):
+
+	def hasMorePayloads():
+
+	def getNextPayload():
+
+	def reset():
+
+	def mutate_payload():
+
+	
+
