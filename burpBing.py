97372240b9624605f0e9d47904267bc987829a45
n1cfury <fukugetin@gmail.com>
2017-02-16 21:36:48 -0800

    Created empty functions.  Complete the code

---
+++ burpBing.py
@@ -0,0 +1,22 @@
+
+
+bing_api_key = ""
+
+def banner():
+	pirnt "  Bing for Burp p88  "
+	print ""
+
+class BurpExtender():
+	
+	def registerExtenderCallbacks():
+
+	def createMenuItems():
+
+	def bing_menu():
+
+	def bing_search():
+
+	def bing_query():
+
+
+
---
+++ burpWordlist.py
@@ -0,0 +1,31 @@
+
+
+def banner():
+	print "Burp Wordlist p94"
+
+class TagStripper():
+
+	def __init__(self):
+
+	def handle_data():
+
+	def handle_comment():
+
+	def striP():
+
+
+class BurpExtender():
+
+	def registerExtenderCAllback():
+
+	def createMenuITems():
+
+	def worldlist_menu():
+
+	def get_words():
+
+	def mangle():
+
+	def display_wordlist():
+
+	
