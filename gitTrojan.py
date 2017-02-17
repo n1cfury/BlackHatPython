4e154407933eeb44819bf262d1845160bc37da63
n1cfury <fukugetin@gmail.com>
2017-02-16 21:38:21 -0800

    Created empty functions.  Complete the code

---
+++ gitTrojan.py
@@ -0,0 +1,32 @@
+import json, base64, sys, time, imp, random, threading, Queue, os
+from github3 import login
+
+
+def banner():
+	print "  GitHub Aware Trojan  p105  "
+	print "You may need to install prerequisites"
+
+trojan_id = "abc"
+
+torjan_config = "%s.json" % trojan_id
+
+
+def connect_to_github():
+
+def get_file():
+
+def get_trojan_config():
+
+def store_module_result():
+
+class GitImporter():
+
+	def __init__self():
+
+	def find_module():
+
+	def load_module():
+
+	def module_runner(module):
+
+	
