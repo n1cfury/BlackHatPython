0519d16a70072a9e8de70cc97886e86a5036bd81
n1cfury <fukugetin@gmail.com>
2017-02-16 21:37:57 -0800

    Created empty functions.  Complete the code

---
+++ ie_exfil.py
@@ -0,0 +1,22 @@
+
+def banner():
+	print "IE COM automation p129"
+
+def wait_for_browser():
+
+def encrypt_string():
+
+def encrypt_post(filename);
+
+def random_sleep():
+
+def login_to_tumblr():
+
+def psot_to_tumblr():
+
+def exfiltrate():
+
+def main():
+
+if __name__ == '__main__':
+	main()
