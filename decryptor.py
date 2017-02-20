03a7c218f568a97764f454c7d73f37e83d3dfed1
n1cfury <fukugetin@gmail.com>
2017-02-19 19:12:41 -0800

    Code completed. DEBUG and TEST

--- decryptor.py
+++ decryptor.py
@@ -2,8 +2,20 @@ import zlib, base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-private key ="paste private key here"
+private_key ="paste private key here"
 
 def banner():
-	print "decryptor p134"
+	print "[***]	Decryptor p134	[***]"
 
+rsakey = RSA.importKey(private_key)
+rsakey = PKCS1_OAEP.new(rsakey)
+chunk_size = 256
+offset = 0
+decrypted = ""
+encrypted = base64.b64decode(encrypted)
+
+while offset < len(encrypted):
+	decrpyted += rsakey.decrypt(encrypted([offset:offset+chunk_size])
+	offset += chunk_size
+plaintext = zlib.decompress(decrypted)
+print plaintext
