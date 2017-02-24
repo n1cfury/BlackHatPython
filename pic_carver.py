6353ca2a980875b3a7dca0e0b1bad5fc6437aae4
n1cfury <fukugetin@gmail.com>
2017-02-24 12:27:32 -0800

    Completed Code.  TEST AND DEBUG

--- pic_carver.py
+++ pic_carver.py
@@ -58,4 +58,14 @@ def extract_image(headers, http_payload):
 					if headers["Content-Encoding"]
 
 
-def face_detect():
+def face_detect(path, file_name):
+	img = cv2.imread(path)
+	cascade = cs2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+	rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+	if len(rects) == 0:
+		return False:
+		rects[:, 2:] += rects[:, :2]
+	for x1,y1,x2,y2 in rects:
+		cv2.rectangle(img,(x1,y1),(x2,y2),(127,255,0),2)
+	cv2.imwrite("%s/%s-%s" % (faces_directory,pcap_file,file_name),img)
+		return True
