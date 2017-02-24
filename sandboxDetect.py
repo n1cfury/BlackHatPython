00a3c81664c5438cca61ccd1d560e7097a3aac52
n1cfury <fukugetin@gmail.com>
2017-02-24 12:44:34 -0800

    Work in Progress.  Continue on pg 119

--- sandboxDetect.py
+++ sandboxDetect.py
@@ -5,12 +5,17 @@ def banner():
 	print "[***]	sandbox detection p118	[***]"
 
 
-class LASTINPUTINFO():
-
+class LASTINPUTINFO(ctypes.Structure):
+	_fields_ = [("cbsize",ctypes.c_uint),("dwTime",ctypes.c_ulong)]
 
 def get_last_input():
+	struct_lastinputinfo = LASTINPUTINFO()
+	struct_lastinputinfo.cbSize = ctypes.sizeof(LASTINPUTINFO)
+	user32.GetLastInputInfo(ctypes.byref(struct_lastinputinfo))
+	run_time = kernel32.GetTickCount()
+	elapsed = run_time = sturct_lastinputinfo.dwTime
+	
 
 def get_key_presses():
 
 def detect_sandbox():
-
