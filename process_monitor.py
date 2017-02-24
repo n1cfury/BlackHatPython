b1597786d55b3a08a23e251e8e66b00030d25a07
n1cfury <fukugetin@gmail.com>
2017-02-24 12:38:50 -0800

    Finished code.  TEST AND DEBUG

--- process_monitor.py
+++ process_monitor.py
@@ -4,3 +4,26 @@ import win32con, win32api, win32security, wmi, sys, os
 def banner():
 	print "[***]	Process MOnitor p140	[***]"
 
+def log_to_file(message):
+	fd = open("process_monitor_log.csv", "ab")
+	fd.write("%s\r\n" % message)
+	fd.close()
+	return
+log_to_file("Time,User,Executable,CommandLine,PID,Parent PID,Privileges")
+c = wmi.WMI()
+process_watcher = c.Win32_Process.watch_for("creation")
+while True:
+	try:
+		new_process = process_watcher()
+		proc_owner = new_process.GetOwner()
+		proc_owner = "%s\\%s" % (proc_owner[0],proc_owner[2])
+		create_date = new_process.CreationDate
+		executable = new_process.ExecutablePath
+		cmdline = new_process.CommaandLine
+		pid = new_process.ProcessId
+		parent_pid = new_process.ParentProcessId
+		privileges = "N/A"
+		process_log_message = "%s,%s,%s,%s,%s,%s,%s\r\n" % (create_date, proc_owner, executable,cmdline,pid,parent_pid,privileges)
+		print process_log_message
+	except:
+		pass
